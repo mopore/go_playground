156288900f4d9baa43e830c6f39a1a93257b54a1
mopore <jni@mopore.org>
2023-08-27 09:32:27 +0200

    Provide an example for a Go MQTT client

---
+++ mqttSimpleSample/README.md
@@ -0,0 +1,21 @@
+```
+ __  __  ___ _____ _____   ____                        _      
+|  \/  |/ _ \_   _|_   _| / ___|  __ _ _ __ ___  _ __ | | ___ 
+| |\/| | | | || |   | |   \___ \ / _` | '_ ` _ \| '_ \| |/ _ \
+| |  | | |_| || |   | |    ___) | (_| | | | | | | |_) | |  __/
+|_|  |_|\__\_\|_|   |_|   |____/ \__,_|_| |_| |_| .__/|_|\___|
+                                                |_|           
+```
+
+How was it created?
+===================
+
+```bash
+mkdir mqtt_example mqttSimpleSample
+cd mqttSimpleSample
+go mod init mqttSimpleSample
+go get -u github.com/eclipse/paho.mqtt.golang
+vim main.go  # Add your code
+go run ./main.go
+```
+
---
+++ mqttSimpleSample/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/eclipse/paho.mqtt.golang"
+)
+
+const (
+    stopic = "jniHome/services/telegramBot/aliveTick"
+    ptopic = "goTest"
+    brokerAddress = "tcp://192.168.199.119:1883"
+    clientId = "goTestClient"
+)
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+    log.Printf("TOPIC: %s\n", msg.Topic())
+    log.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+    opts := mqtt.NewClientOptions().AddBroker(brokerAddress)
+    opts.SetClientID(clientId)
+    opts.SetKeepAlive(2 * time.Second)
+    opts.SetDefaultPublishHandler(f)
+    opts.SetPingTimeout(1 * time.Second)
+
+    c := mqtt.NewClient(opts)
+    if token := c.Connect(); token.Wait() && token.Error() != nil {
+        panic(token.Error())
+    }
+
+    if token := c.Subscribe(stopic, 0, nil); token.Wait() && token.Error() != nil{
+        panic(token.Error())
+    }
+
+    for i := 0; i < 5; i++ {
+        text := fmt.Sprintf("This is msg #%d!", i)
+        token := c.Publish(ptopic, 0, false, text)
+        token.Wait()
+    }
+
+    time.Sleep(6 * time.Second)
+
+    if token := c.Unsubscribe(stopic); token.Wait() && token.Error() != nil {
+        panic(token.Error())
+    }
+
+    c.Disconnect(250)
+
+    time.Sleep(1 * time.Second)
+    log.Println("Program done")
+}
