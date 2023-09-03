3488250b591430395a511838bff7e1d74e506826
mopore <jni@mopore.org>
2023-09-03 13:11:00 +0200

    Extend example to support return values from routines.

--- learningGo/waitForRoutines/main.go
+++ learningGo/waitForRoutines/main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
-    "time"
+	"time"
 )
 
-func doStuff() {
-    time.Sleep(500 * time.Millisecond)
+const routineCount = 3
+
+func doStuff() int {
+    // Create a random number between 100 and 500
+    r := rand.Intn(400) + 100
+    time.Sleep(time.Duration(r) * time.Millisecond)
+    return r
 }
 
 func main() {
-    fmt.Println("Starting...")
+    fmt.Printf("Preparing to run %d goroutines.\n", routineCount)
     var wg sync.WaitGroup
-    wg.Add(3)
-    for i := 0; i < 3; i++ {
+    results := make(chan int, routineCount)
+    wg.Add(routineCount)
+    for i := 0; i < routineCount; i++ {
         go func(i int) {
             defer wg.Done()
             fmt.Printf("Goroutine #%d\n", i)
-            doStuff()
+            r := doStuff()
+            results <- r
         }(i)
     }
     wg.Wait()
-    fmt.Println("All done")
+    close(results)
+    fmt.Println("All routines completed.")
+    for r := range results {
+        fmt.Printf("Got result: %d\n", r)
+    }
 }
