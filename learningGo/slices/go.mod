f8ef1c6b0254ebf0acd81ac9a487267f2431f04e
mopore <jni@mopore.org>
2023-09-03 09:21:31 +0200

    Add an example on how to handle slices.

---
+++ learningGo/slices/go.mod
@@ -0,0 +1,3 @@
+module jnislices
+
+go 1.21.0
---
+++ learningGo/slices/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+)
+
+func describeSlice(name string, s []int) {
+    fmt.Printf("name: %s\n", name)
+    fmt.Printf("len: %d, cap: %d\n", len(s), cap(s))
+    for _, v := range s {
+        fmt.Printf("v: %d\n", v)
+    }
+}
+
+func main() {
+    s := []int{1, 2, 3, 4, 5}
+    s = append(s, 6)
+    describeSlice("Orignal", s)
+    clone := make([]int, len(s))
+    copy(clone, s)
+    // Remove the second element
+    clone = append(clone[:1], clone[2:]...)
+    describeSlice("Clone without second", clone) 
+}
