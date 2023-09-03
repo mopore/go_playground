ca62383be305b3078c39f888ed8f1cbf7af82923
mopore <jni@mopore.org>
2023-09-03 18:02:21 +0200

    Clean up code and log messages.

--- mqttsimplesample/main/main.go
+++ mqttsimplesample/main/main.go
@@ -3,11 +3,19 @@ package main
 import (
     "log"
     "mqttsimplesample/mqtt"
+    "sync"
 )
 
 func main() {
-    log.Println("Entry of main...")
-    mqtt.PerformMqttRun()
-    log.Println("Exit of main.")
+    log.Println("Main start.")
+
+    var wg sync.WaitGroup
+    wg.Add(1)
+    go mqtt.RunMqtt(&wg)
+
+    log.Println("Main waits now...")
+    wg.Wait()
+
+    log.Println("Main exit.")
 }
 
--- mqttsimplesample/mqtt/mqtt.go
+++ mqttsimplesample/mqtt/mqtt.go
@@ -3,54 +3,82 @@ package mqtt
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/eclipse/paho.mqtt.golang"
 )
 
 const (
-    stopic = "jniHome/services/telegramBot/aliveTick"
-    ptopic = "goTest"
+    aliveTopic = "goTest/services/mqttBridge/aliveTick"
+    commandTopic = "goTest/services/mqttBridge/command"
+    testTopic = "goTest/services/mqttBridge/test"
     brokerAddress = "tcp://192.168.199.119:1883"
     clientId = "goTestClient"
+    aliveFrequency = 10 * time.Second
 )
 
-var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-    log.Printf("TOPIC: %s\n", msg.Topic())
-    log.Printf("MSG: %s\n", msg.Payload())
+func publish5TestMessages(c mqtt.Client) {
+    log.Printf("Publishing 5 test messages on topic %s...\n", testTopic)
+    for i := 0; i < 5; i++ {
+        text := fmt.Sprintf("This is msg #%d!", i+1)
+        token := c.Publish(testTopic, 0, false, text)
+        token.Wait()
+    }
 }
 
-func PerformMqttRun() {
-    log.Println("Starting MQTT run")
+func RunMqtt(wg *sync.WaitGroup) {
+    log.Println("Preparing MQTT client...")
+    cdone := make(chan struct{})
+
     opts := mqtt.NewClientOptions().AddBroker(brokerAddress)
     opts.SetClientID(clientId)
     opts.SetKeepAlive(2 * time.Second)
-    opts.SetDefaultPublishHandler(f)
     opts.SetPingTimeout(1 * time.Second)
 
+    var ph mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+        if msg.Topic() == commandTopic && string(msg.Payload()) == "EXIT" {
+            log.Printf("Got EXIT message on topic \"%s\"", aliveTopic)
+            close(cdone)
+        }
+    }
+    opts.SetDefaultPublishHandler(ph)
+
     c := mqtt.NewClient(opts)
+    log.Println("Connecting MQTT client...")
     if token := c.Connect(); token.Wait() && token.Error() != nil {
         panic(token.Error())
     }
+    log.Println("Connected MQTT client")
 
-    if token := c.Subscribe(stopic, 0, nil); token.Wait() && token.Error() != nil{
+    publish5TestMessages(c)
+
+    if token := c.Subscribe(commandTopic, 0, nil); token.Wait() && token.Error() != nil{
         panic(token.Error())
     }
+    log.Printf("Send 'EXIT' to topic %s to exit\n", commandTopic)
 
-    for i := 0; i < 5; i++ {
-        text := fmt.Sprintf("This is msg #%d!", i)
-        token := c.Publish(ptopic, 0, false, text)
-        token.Wait()
+    go func() {
+        text := "ALIVE"
+        log.Printf("Starting ALIVE Publishing \"%s\" on topic %s...\n", text, aliveTopic)
+        for {
+            token := c.Publish(aliveTopic, 0, false, text)
+            token.Wait()
+            time.Sleep(aliveFrequency)
+        }
+    }()
+    select {
+    case <-cdone:
+        log.Println("Exiting MQTT ALIVE publishing routine...")
     }
 
-    time.Sleep(6 * time.Second)
-
-    if token := c.Unsubscribe(stopic); token.Wait() && token.Error() != nil {
+    if token := c.Unsubscribe(aliveTopic); token.Wait() && token.Error() != nil {
         panic(token.Error())
     }
 
     c.Disconnect(250)
 
     time.Sleep(1 * time.Second)
-    log.Println("Finishing MQTT run without errors")
+    log.Println("Finishing MQTT client gracefully...")
+    wg.Done()
 }
