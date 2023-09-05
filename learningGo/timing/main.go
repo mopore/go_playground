e864ec2c84e89ff0338a8d4b07cd5444ec96343d
mopore <jni@mopore.org>
2023-09-05 21:05:33 +0200

    Provide an example for using timing functions

---
+++ learningGo/timing/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+    now := time.Now()
+
+    log.Println("Current time is: ", now.Format("2006-01-02 15:04:05"))
+
+    time.AfterFunc(2*time.Second, func() {
+        log.Println("2 seconds later than above...")
+    })
+    time.Sleep(3 * time.Second)
+
+    total := time.Since(now)
+    log.Println("Total time in seconds: ", total.Seconds())
+}
+
