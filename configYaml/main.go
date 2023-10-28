a922cc7b2692edad571444e0c909b9c894bf6f2d
mopore <jni@mopore.org>
2023-10-28 23:12:20 +0200

    Read Yaml config with Viper

--- configYaml/main.go
+++ configYaml/main.go
@@ -1,39 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 
-	"gopkg.in/yaml.v3"
+    "gopkg.in/yaml.v3"
+    "github.com/spf13/viper"
 )
 
-type MyConfig struct {
-    // Remember to have the field name start with a capital letter
-    ApiVersion string `yaml:"apiVersion"`
-    Database struct {
-        Host string `yaml:"host"` 
-        Port int `yaml:"port"`
-        User string `yaml:"user"`
-        Password string `yaml:"password"`
-        Name string `yaml:"name"`
-    } `yaml:"database"`
+
+func readWithViper() {
+    viper.SetConfigFile("config.yaml")
+    viper.ReadInConfig()
+    host := viper.GetString("database.host")
+    log.Printf("Host from viper: %s\n", host)
 }
 
 
-func main() {
+func readWithStruct() {
+
     content, err := os.ReadFile("config.yaml")
     if err != nil {
         log.Fatal(err)
     }
 
-    var config MyConfig
+    var config struct {
+        ApiVersion string `yaml:"apiVersion"`
+        Database struct {
+            Host string `yaml:"host"` 
+            Port int `yaml:"port"`
+            User string `yaml:"user"`
+            Password string `yaml:"password"`
+            Name string `yaml:"name"`
+        } `yaml:"database"`
+    }
 
     err = yaml.Unmarshal(content, &config)
     if err != nil {
         log.Fatal(err)
     }
 
-    fmt.Printf("Config: %v\n", config)
+    fmt.Printf("Host from scruct: %v\n", config.Database.Host)
 }
 
+
+func main() {
+    readWithViper()
+    fmt.Println("--------------------------------------------------")
+    readWithStruct()
+}
