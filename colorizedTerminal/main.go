9eca8e88e6cd9f5b4f71f1bdffacd0bbb1b297e5
mopore <jni@mopore.org>
2023-10-29 12:29:09 +0100

    Add an example for colorized terminals.

---
+++ colorizedTerminal/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gookit/color"
+)
+
+func main() {
+    color.Bold.Println("Bold line")
+	color.Red.Println("Simple to use color")
+	color.Green.Print("Simple to use color\n")
+	color.Cyan.Printf("Simple to use %s\n", "color")
+	color.Yellow.Printf("Simple to use %s\n", "color")
+
+	// use like func
+	red := color.FgRed.Render
+	green := color.FgGreen.Render
+
+	fmt.Printf("%s line %s library\n", red("Command"), green("color"))
+}
+
+
