fb13a38275d28ac85e461741c1cd27c965e1a1f3
mopore <jni@mopore.org>
2023-10-30 18:53:20 +0100

    Correct intendation

--- colorizedTerminal/main.go
+++ colorizedTerminal/main.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
     color.Bold.Println("Bold line")
-	color.Red.Println("Simple to use color")
-	color.Green.Print("Simple to use color\n")
-	color.Cyan.Printf("Simple to use %s\n", "color")
-	color.Yellow.Printf("Simple to use %s\n", "color")
+    color.Red.Println("Simple to use color")
+    color.Green.Print("Simple to use color\n")
+    color.Cyan.Printf("Simple to use %s\n", "color")
+    color.Yellow.Printf("Simple to use %s\n", "color")
 
-	// use like func
-	red := color.FgRed.Render
-	green := color.FgGreen.Render
+    // use like func
+    red := color.FgRed.Render
+    green := color.FgGreen.Render
 
-	fmt.Printf("%s line %s library\n", red("Command"), green("color"))
+    fmt.Printf("%s line %s library\n", red("Command"), green("color"))
 }
 
 
