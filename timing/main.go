5e21e4f7f7832d425673b696314794958d715fe5
mopore <jni@mopore.org>
2024-05-19 18:00:02 +0200

    Cosmetics.

--- timing/main.go
+++ timing/main.go
@@ -6,16 +6,15 @@ import (
 )
 
 func main() {
-    now := time.Now()
+	now := time.Now()
 
-    log.Println("Current time is: ", now.Format("2006-01-02 15:04:05"))
+	log.Println("Current time is: ", now.Format("2006-01-02 15:04:05"))
 
-    time.AfterFunc(2*time.Second, func() {
-        log.Println("2 seconds later than above...")
-    })
-    time.Sleep(3 * time.Second)
+	time.AfterFunc(2*time.Second, func() {
+		log.Println("2 seconds later than above...")
+	})
+	time.Sleep(3 * time.Second)
 
-    total := time.Since(now)
-    log.Println("Total time in seconds: ", total.Seconds())
+	total := time.Since(now)
+	log.Println("Total time in seconds: ", total.Seconds())
 }
-
