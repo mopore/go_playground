5969124c0f7c633978ac6d47ec22ea1dc5a72c42
mopore <jni@mopore.org>
2024-05-19 09:25:17 +0200

    Clean up and refactor repo.

--- example1/example1.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	message := "Hello, world!"
-	fmt.Printf("Message from Example1: %s\n", message)
-}
--- helloWorld/go.mod
+++
@@ -1,3 +0,0 @@
-module tut/helloworld
-
-go 1.21.5
