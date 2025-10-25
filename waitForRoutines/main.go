4238f0667c4ee7939f1414adcd172b3002fa5423
mopore <jni@mopore.org>
2025-10-25 10:39:02 +0200

    Update go routine syntax to go 1.25

--- waitForRoutines/main.go
+++ waitForRoutines/main.go
@@ -7,28 +7,40 @@ import (
 	"time"
 )
 
-const routineCount = 3
+const rCount = 6
 
 func doStuff() int {
     // Create a random number between 100 and 500
-    r := rand.Intn(400) + 100
+    r := rand.Intn(500-100) + 100
     time.Sleep(time.Duration(r) * time.Millisecond)
     return r
 }
 
 func main() {
-    fmt.Printf("Preparing to run %d goroutines.\n", routineCount)
+    fmt.Printf("Preparing to run %d goroutines.\n", rCount)
     var wg sync.WaitGroup
-    results := make(chan int, routineCount)
-    wg.Add(routineCount)
-    for i := 0; i < routineCount; i++ {
-        go func(i int) {
-            defer wg.Done()
+    results := make(chan int, rCount)
+
+    // // old syntax
+    // wg.Add(rCount)
+    // for i := 0; i < rCount; i++ {
+    //     go func(i int) {
+    //         defer wg.Done()
+    //         fmt.Printf("Goroutine #%d\n", i)
+    //         r := doStuff()
+    //         results <- r
+    //     }(i)
+    // }
+
+    // NEW syntax
+    for i := range rCount {
+        wg.Go(func() {
             fmt.Printf("Goroutine #%d\n", i)
             r := doStuff()
             results <- r
-        }(i)
+        })
     }
+
     wg.Wait()
     close(results)
     fmt.Println("All routines completed.")
