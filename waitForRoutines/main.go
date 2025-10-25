009365244978e569b210039d483985c7b532b2f7
mopore <jni@mopore.org>
2025-10-25 14:49:18 +0200

    Cosmetics.

--- waitForRoutines/main.go
+++ waitForRoutines/main.go
@@ -10,39 +10,28 @@ import (
 const rCount = 6
 
 func doStuff() int {
-    // Create a random number between 100 and 500
-    r := rand.Intn(500-100) + 100
+    r := rand.Intn(500-100) + 100  // Create a random number between 100 and 500
     time.Sleep(time.Duration(r) * time.Millisecond)
     return r
 }
 
 func main() {
     fmt.Printf("Preparing to run %d goroutines.\n", rCount)
+
     var wg sync.WaitGroup
     results := make(chan int, rCount)
 
-    // // old syntax
-    // wg.Add(rCount)
-    // for i := 0; i < rCount; i++ {
-    //     go func(i int) {
-    //         defer wg.Done()
-    //         fmt.Printf("Goroutine #%d\n", i)
-    //         r := doStuff()
-    //         results <- r
-    //     }(i)
-    // }
-
-    // NEW syntax
     for i := range rCount {
+        // Avoid old syntax with WaitGroup.Add and Waitgroup.Done
         wg.Go(func() {
             fmt.Printf("Goroutine #%d\n", i)
             r := doStuff()
             results <- r
         })
     }
-
     wg.Wait()
     close(results)
+
     fmt.Println("All routines completed.")
     for r := range results {
         fmt.Printf("Got result: %d\n", r)
